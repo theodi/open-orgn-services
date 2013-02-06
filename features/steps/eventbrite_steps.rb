edf277392c9e7d4f8b8e5e9b5b71f15f1e3b11ca
James Smith <james@floppy.org.uk>
2013-02-06 21:26:43 +0000

    Refactor cukes slightly and add feature to check for attendees who have already paid.
    0:20

--- features/steps/eventbrite_steps.rb
+++ features/steps/eventbrite_steps.rb
@@ -1,26 +1,46 @@
 require 'event_monitor'
 
-Given /^there is an event in Eventbrite$/ do
+Given /^there is an event in Eventbrite with id (\d+)$/ do |id|
+  # Set up eventbrite monitor
+  @e = EventMonitor.new event_id: id
 end
 
-When /^I sign up to that event and ask to be invoiced$/ do
-  # Set up some user details
+When /^'(.*)' signs up to that event and asks to be invoiced$/ do |email|
+  # User details are entered on Eventbrite, and price is worked out
+  @email = email
+  @price = 0.66
 end
 
-Then /^I should be marked as wanting an invoice$/ do
-  # Mock response from eventbrite API
-  # https://www.eventbrite.co.uk/myevent?eid=5441375300
-  # Poll eventbrite API for details
-  # Find the relevant user and check the order type field
-  Resque.should_receive(:enqueue).with(AttendeeInvoicer, 'james.smith@theodi.org', 0.66)
-  VCR.use_cassette('synopsis') do
-    e = EventMonitor.new event_id: 5441375300
-    e.check_invoices
+When /^'(.*)' signs up to that event and gets a free ticket$/ do |email|
+  # User details are entered on Eventbrite, and price is worked out
+  @email = email  
+  @price = 0
+end
+
+Then /^he should be added to the invoicing queue$/ do
+  # Set expectation
+  Resque.should_receive(:enqueue).with(AttendeeInvoicer, @email, @price)
+  # Check the attendees
+  VCR.use_cassette('needs_invoice') do
+    @e.check_invoices
   end
 end
 
+Then /^he should not be added to the invoicing queue$/ do
+  # Set expectation
+  Resque.should_not_receive(:enqueue)
+  # Check the attendees
+  VCR.use_cassette('does_not_need_invoice') do
+    @e.check_invoices
+  end
+end
+
+Given /^'(.*)' needs to be invoiced for ([\d\.]+)$/ do |email, price|
+end
+
 Given /^I have asked to be invoiced$/ do
-  pending # express the regexp above with the code you wish you had
+  @email = email
+  @price = price.to_f
 end
 
 Then /^an invoice should be raised in Xero$/ do
