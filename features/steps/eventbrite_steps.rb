f5e022ae8ca59688d3f1e4942dc142e20a5234d8
James Smith <james@floppy.org.uk>
2013-02-06 12:56:59 +0000

    Add feature and a couple of simple scenarios for invoicing
    0:15

---
+++ features/steps/eventbrite_steps.rb
@@ -0,0 +1,14 @@
+Given /^there is an event in Eventbrite$/ do
+end
+
+When /^I sign up to that event and ask to be invoiced$/ do
+  # Set up some user details
+end
+
+Then /^I should be marked as wanting an invoice$/ do
+  # Mock response from eventbrite API
+  # https://www.eventbrite.co.uk/myevent?eid=5441375300
+  # Poll eventbrite API for details
+  # Find the relevant user and check the order type field
+  pending # express the regexp above with the code you wish you had
+end
