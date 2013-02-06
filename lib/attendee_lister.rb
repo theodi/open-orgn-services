8dc72ebb6209446ca92c3e7dc105cab041de76a4
James Smith <james@floppy.org.uk>
2013-02-06 21:35:43 +0000

    Refactor EventMonitor into a resque job called AttendeeLister

--- lib/attendee_lister.rb
+++ lib/attendee_lister.rb
@@ -1,14 +1,13 @@
+require 'resque'
 require 'eventbrite-client'
 require 'attendee_invoicer'
 
-class EventMonitor
-  def initialize options = {}
-    @event_id = options[:event_id]
-  end
+class AttendeeLister
+  @queue = :invoicing
 
-  def check_invoices
+  def self.perform(event_id)
     e = EventbriteClient.new ({ :app_key => ENV["EVENTBRITE_API_KEY"], :user_key => ENV["EVENTBRITE_USER_KEY"]})
-    if response = e.event_list_attendees(id: @event_id, page: 0)
+    if response = e.event_list_attendees(id: event_id, page: 0)
       response.parsed_response['attendees'].each do |attendee|
         a = attendee['attendee']
         if a['order_type'] == "Send an Invoice - Payment Not Received"
