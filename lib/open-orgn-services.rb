b4152ab0a275d1e51825f1e8cbe2fd39af47c1ae
pikesley <github@orgraphone.org>
2013-02-07 15:18:15 +0000

    Work out Xero API, first cut of scenarios
    2:00 with @Floppy

--- lib/open-orgn-services.rb
+++ lib/open-orgn-services.rb
@@ -1,6 +1,8 @@
 require 'resque'
+require 'xeroizer'
 
 require 'eventbrite-client'
 require 'eventbrite/event_lister'
 require 'eventbrite/attendee_lister'
 require 'eventbrite/attendee_invoicer'
+
