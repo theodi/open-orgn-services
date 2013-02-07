bf548806108a7f9093b3cd3f2ca026adff363878
pikesley <github@orgraphone.org>
2013-02-07 15:13:52 +0000

    Resolve merge conflict

--- features/steps/eventbrite_steps.rb
+++ features/steps/eventbrite_steps.rb
@@ -61,11 +61,36 @@ When /^I sign up to that event and get a free ticket$/ do
   end
 end
 
-Given /^I have asked to be invoiced$/ do
+
+Given /^'(.*)' needs to be invoiced for ([\d\.]+)$/ do |email, price|
   @email = email
   @price = price.to_f
 end
 
-Then /^an invoice should be raised in Xero$/ do
-  pending # express the regexp above with the code you wish you had
+When /^the attendee invoicer runs$/ do
+  VCR.use_cassette('raise_invoice_in_xero') do
+    AttendeeInvoicer.perform(@email, @price)
+  end
+end
+
+Given /^'james\.smith@theodi\.org' already exists as a contact in Xero$/ do
+  VCR.use_cassette('xero_contact_lookup') do
+    xero = Xeroizer::PrivateApplication.new(
+        ENV["XERO_CONSUMER_KEY"],
+        ENV["XERO_CONSUMER_SECRET"],
+        ENV["XERO_PRIVATE_KEY_PATH"]
+    )
+    xero.Contact.all(:where => 'EmailAddress == "james.smith@theodi.org"') != []
+  end
+end
+
+Given /^'tom\.heath@theodi\.org' does not already exist as a contact in Xero$/ do
+  VCR.use_cassette('xero_contact_inverse_lookup') do
+    xero = Xeroizer::PrivateApplication.new(
+        ENV["XERO_CONSUMER_KEY"],
+        ENV["XERO_CONSUMER_SECRET"],
+        ENV["XERO_PRIVATE_KEY_PATH"]
+    )
+    xero.Contact.all(:where => 'EmailAddress == "tom.heath@theodi.org"') == []
+  end
 end
