053fc5a648736c4d9c785917b214e00c1e3ff702
pikesley <github@orgraphone.org>
2013-02-07 18:52:14 +0000

    Oops, shouldn't have done that

--- features/support/env.rb
+++ features/support/env.rb
@@ -16,6 +16,10 @@ require 'open-orgn-services'
 VCR.configure do |c|
   c.filter_sensitive_data("<EVENTBRITE_API_KEY>") { ENV['EVENTBRITE_API_KEY'] }
   c.filter_sensitive_data("<EVENTBRITE_USER_KEY>") { ENV['EVENTBRITE_USER_KEY'] }
+  c.filter_sensitive_data("<EVENTBRITE_ORGANISER_ID>") { ENV['EVENTBRITE_ORGANISER_ID'] }
+  c.filter_sensitive_data("<XERO_CONSUMER_KEY>") { ENV['XERO_CONSUMER_KEY'] }
+  c.filter_sensitive_data("<XERO_CONSUMER_SECRET>") { ENV['XERO_CONSUMER_SECRET'] }
+  c.filter_sensitive_data("<XERO_PRIVATE_KEY_PATH>") { ENV['XERO_PRIVATE_KEY_PATH'] }
   c.cassette_library_dir = 'fixtures/vcr_cassettes'
   c.hook_into :webmock # or :fakeweb
 end
