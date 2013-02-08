e5718f97427db645727f9140835cdf9d42be6201
James Smith <james@floppy.org.uk>
2013-02-08 16:30:07 +0000

    Auto-filter sensitive keys
    0:05

--- features/support/env.rb
+++ features/support/env.rb
@@ -22,13 +22,11 @@ Spork.each_run do
   require 'open-orgn-services'
 
   VCR.configure do |c|
-    c.filter_sensitive_data("<EVENTBRITE_API_KEY>") { ENV['EVENTBRITE_API_KEY'] }
-    c.filter_sensitive_data("<EVENTBRITE_USER_KEY>") { ENV['EVENTBRITE_USER_KEY'] }
-    c.filter_sensitive_data("<EVENTBRITE_ORGANISER_ID>") { ENV['EVENTBRITE_ORGANISER_ID'] }
-    c.filter_sensitive_data("<XERO_CONSUMER_KEY>") { ENV['XERO_CONSUMER_KEY'] }
-    c.filter_sensitive_data("<XERO_CONSUMER_SECRET>") { ENV['XERO_CONSUMER_SECRET'] }
-    c.filter_sensitive_data("<XERO_PRIVATE_KEY_PATH>") { ENV['XERO_PRIVATE_KEY_PATH'] }
+    # Automatically filter all secure details that are stored in the environment
+    Dotenv.load.keys.each do |key|
+      c.filter_sensitive_data("<#{key}>") { ENV[key] }
+    end
     c.cassette_library_dir = 'fixtures/vcr_cassettes'
-    c.hook_into :webmock # or :fakeweb
+    c.hook_into :webmock
   end
 end
