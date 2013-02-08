20135a304fadc161a2680fac1aa2a22bd634d316
James Smith <james@floppy.org.uk>
2013-02-08 10:45:27 +0000

    Added doc block for EventLister

--- lib/eventbrite/event_lister.rb
+++ lib/eventbrite/event_lister.rb
@@ -1,6 +1,14 @@
 class EventLister
   @queue = :invoicing
 
+  # Public: Inspect the list of event on Eventbrite
+  #
+  # Examples
+  #
+  #   EventLister.perform
+  #   # => nil
+  #
+  # Returns nil. Queues further jobs to handle inspection of attendee lists.
   def self.perform
     e = EventbriteClient.new ({ :app_key => ENV["EVENTBRITE_API_KEY"], :user_key => ENV["EVENTBRITE_USER_KEY"]})
     if response = e.organizer_list_events(id: ENV['EVENTBRITE_ORGANIZER_ID'])
