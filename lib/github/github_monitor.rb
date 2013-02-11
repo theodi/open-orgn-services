189ccd22fb6cea30896322d6d32e5fbc80ffd958
James Smith <james@floppy.org.uk>
2013-02-11 09:30:44 +0000

    Fix issue count in github monitor

--- lib/github/github_monitor.rb
+++ lib/github/github_monitor.rb
@@ -12,13 +12,13 @@ class GithubMonitor
     # Public repos into leftronic
     Resque.enqueue LeftronicPublisher, :number, ENV['LEFTRONIC_GITHUB_REPOS'], org.public_repos
     # Count up stats across all repositories
-    issues = 0
+    open_issues = 0
     watchers = 0
     forks = 0
     open_pull_requests = 0
     pull_requests = 0
     github.repos.list(user: ENV['GITHUB_ORGANISATION']) do |repo|
-      issues += repo.open_issues_count
+      open_issues += repo.open_issues_count
       watchers += repo.watchers_count
       forks += repo.forks_count
       issues = github.issues.list(user: ENV['GITHUB_ORGANISATION'], repo: repo.name)
@@ -27,7 +27,7 @@ class GithubMonitor
       pull_requests += issues.select{|x| x["pull_request"] && x["pull_request"]["html_url"]}.count
     end
     # Push into leftronic
-    Resque.enqueue LeftronicPublisher, :number, ENV['LEFTRONIC_GITHUB_ISSUES'], issues
+    Resque.enqueue LeftronicPublisher, :number, ENV['LEFTRONIC_GITHUB_ISSUES'], open_issues
     Resque.enqueue LeftronicPublisher, :number, ENV['LEFTRONIC_GITHUB_WATCHERS'], watchers
     Resque.enqueue LeftronicPublisher, :number, ENV['LEFTRONIC_GITHUB_FORKS'], forks
     Resque.enqueue LeftronicPublisher, :number, ENV['LEFTRONIC_GITHUB_OPENPRS'], open_pull_requests
