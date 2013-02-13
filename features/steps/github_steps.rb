b3bf2135d17d4514b7c3a3f1d69de357f5c4c98b
James Smith <james@floppy.org.uk>
2013-02-13 09:47:23 +0000

    Add tests for Trello metrics
    0:10

--- features/steps/github_steps.rb
+++ features/steps/github_steps.rb
@@ -50,19 +50,3 @@ When /^the github monitor runs$/ do
     GithubMonitor.perform
   end
 end
-
-def statistic_id(stat_name)
-  ids = {
-    'public repositories' => 'LEFTRONIC_GITHUB_REPOS',
-    'open issues' => 'LEFTRONIC_GITHUB_ISSUES',
-    'watchers' => 'LEFTRONIC_GITHUB_WATCHERS',
-    'forks' => 'LEFTRONIC_GITHUB_FORKS',
-    'open pull requests' => 'LEFTRONIC_GITHUB_OPENPRS',
-    'total pull requests' => 'LEFTRONIC_GITHUB_PULLS',
-  }
-  if ids[stat_name]
-    ENV[ids[stat_name]]
-  else
-    raise ArgumentError.new("Unknown stat #{stat_name}")
-  end
-end
