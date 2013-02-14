cd27500974eaee090febdc9cfa27cf216de1f985
James Smith <james@floppy.org.uk>
2013-02-14 13:59:06 +0000

    Add a DATETIME transform
    0:05

--- features/support/transforms.rb
+++ features/support/transforms.rb
@@ -9,3 +9,7 @@ end
 DATE = Transform /^(\d+)\-(\d+)\-(\d+)$/ do |year, month, day|
   Date.new(year.to_i, month.to_i, day.to_i)
 end
+
+DATETIME = Transform /^(\d+)\-(\d+)\-(\d+) (\d+):(\d+)$/ do |year, month, day, hour, minute|
+  DateTime.new(year.to_i, month.to_i, day.to_i, hour.to_i, minute.to_i)
+end
