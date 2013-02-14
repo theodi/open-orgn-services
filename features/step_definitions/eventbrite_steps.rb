5f6855954364136d4325feaf792efca835172e5a
James Smith <james@floppy.org.uk>
2013-02-14 14:13:01 +0000

    Working JSON generator scenario
    Oh god this is ugly. I will refactor, I promise. 1:00

--- features/step_definitions/eventbrite_steps.rb
+++ features/step_definitions/eventbrite_steps.rb
@@ -33,6 +33,40 @@ Given /^the gross price of the event in Eventbrite is (#{FLOAT})$/ do |price|
   @gross_price = price
 end
 
+Given /^that event has a url '(.*?)'$/ do |url|
+  @events.last[:url] = url
+end
+
+Given /^that event starts at (#{DATETIME})$/ do |datetime|
+  @events.last[:starts_at] = datetime
+end
+
+Given /^that event ends at (#{DATETIME})$/ do |datetime|
+  @events.last[:ends_at] = datetime
+end
+
+Given /^that event is being held at '(.*?)'$/ do |location|
+  @events.last[:location] = location
+end
+
+Given /^that event has (#{INTEGER}) tickets called "(.*?)" which cost ([A-Z]{3}) (#{FLOAT})$/ do |tickets, name, currency, price|
+  @events.last[:ticket_types] ||= []
+  @events.last[:ticket_types] << {
+    :remaining => tickets,
+    :name => name,
+    :price => price,
+    :currency => currency
+  }
+end
+
+Given /^that ticket type is on sale from (#{DATETIME})$/ do |datetime|
+  @events.last[:ticket_types].last[:starts_at] = datetime
+end
+
+Given /^that ticket type is on sale until (#{DATETIME})$/ do |datetime|
+@events.last[:ticket_types].last[:ends_at] = datetime
+end
+
 Then /^the net price of the event is (#{FLOAT})$/ do |price|
   @net_price = price
 end
--- lib/eventbrite/event_summary_generator.rb
+++ lib/eventbrite/event_summary_generator.rb
@@ -3,48 +3,51 @@ class EventSummaryGenerator
   @queue = :invoicing
   
   def self.perform(events)
-    data = {
-      "http://eventbrite.co.uk/..." => {
-          "@type" => "http://schema.org/Event",
-          "startDate" => "2013-04-29T09:00:00",
-          "endDate" => "2013-05-01T17:00:00",
-          "location" => {
-            "@type" => "http://schema.org/Place",
-            "name" => "Covent Garden, London",
-            "address" => {
-              "@type" => "http://schema.org/PostalAddress",
-              "name" => "Wallspace Covent Garden",
-              "streetAddress" => "2 Dryden Street",
-              "addressLocality" => "Covent Garden",
-              "postalCode" => "WC2E 9NA",
-              "addressCountry" => "UK"
-            },
-            "geo" => {
-              "latitude" => 51.514495,
-              "longitude" => -0.123028
-            }
-          },
-          "offers" => [{
-            "@type" => "http://schema.org/Offer",
-            "name" => "Standard Registration",
-            "price" => 1395.00,
-            "priceCurrency" => "GBP",
-            "validFrom" => "2013-03-29",
-            "inventoryLevel" => 13
-          }, {
-            "@type" => "http://schema.org/Offer",
-            "name" => "Early-Bird Registration",
-            "price" => 1255.00,
-            "priceCurrency" => "GBP",
-            "validThrough" => "2013-03-29",
-            "inventoryLevel" => 13
-          }]
-        }, 
-      "http://eventbrite.com/..." => {
-        "@type" => "http://schema.org/Event"
+    data = {}
+    events.each do |event|
+      # Get ticket info
+      tickets = []
+      if event[:ticket_types]
+        event[:ticket_types].each do |ticket_type|
+          tickets << {
+            :@type => "http://schema.org/Offer",
+            :name => ticket_type[:name],
+            :price => ticket_type[:price],
+            :priceCurrency => ticket_type[:currency],
+            :validFrom => ticket_type[:starts_at],
+            :validThrough => ticket_type[:ends_at],
+            :inventoryLevel => ticket_type[:remaining]
+          }
+        end
+      end
+      # Marshal all event data
+      data[event[:url]] = {
+        :@type => "http://schema.org/Event",
+        :startDate => event[:starts_at],
+        :endDate => event[:ends_at],
+        :location => {
+          :@type => "http://schema.org/Place",
+          :name => event[:location],
+          # :address => {
+          #   :@type => "http://schema.org/PostalAddress",
+          #   :name => nil,
+          #   :streetAddress => nil,
+          #   :addressLocality => nil,
+          #   :postalCode => nil,
+          #   :addressCountry => nil
+          # },          
+        },
+        # :geo => {
+        #   :latitude => nil,
+        #   :longitude => nil
+        # },
+        :offers => tickets
       }
-    }
-    Resque.enqueue(EventSummaryUploader, JSON.pretty_generate(data, :indent => '  '))
+    end
+    # Generate JSON
+    json = JSON.pretty_generate(data, :indent => '  ')
+    # Enqueue
+    Resque.enqueue(EventSummaryUploader, json)
   end
   
 end
