49b372d690f447e1c3c6aea3f9d93659f392f2be
James Smith <james@floppy.org.uk>
2013-02-14 09:36:44 +0000

    Pass more event details between jobs
    0:15

--- lib/eventbrite/attendee_lister.rb
+++ lib/eventbrite/attendee_lister.rb
@@ -4,7 +4,10 @@ class AttendeeLister
 
   # Public: Inspect the list of attendees for an event
   #
-  # event_id - the ID of the event in Eventbrite
+  # event_detais - a hash containing the details of the event from Eventbrite
+  #                :id - the eventbrite ID
+  #                :title - the title of the event
+  #                :date - the start date of the event 
   #
   # Examples
   #
@@ -12,13 +15,17 @@ class AttendeeLister
   #   # => nil
   #
   # Returns nil. Queues further jobs to handle invoice raising.
-  def self.perform(event_id)
+  def self.perform(event_details)
     e = EventbriteClient.new ({ :app_key => ENV["EVENTBRITE_API_KEY"], :user_key => ENV["EVENTBRITE_USER_KEY"]})
-    if response = e.event_list_attendees(id: event_id, page: 0)
+    if response = e.event_list_attendees(id: event_details[:id], page: 0)
       response.parsed_response['attendees'].each do |attendee|
         a = attendee['attendee']
         if a['order_type'] == "Send an Invoice - Payment Not Received"
-          Resque.enqueue(AttendeeInvoicer, {:email => a['email']}, {:id => event_id}, {:price => a['amount_paid'].to_f/1.2})
+          Resque.enqueue(AttendeeInvoicer, 
+            {:email => a['email']}, 
+            event_details, 
+            {:price => a['amount_paid'].to_f/1.2}
+          )
         end
       end
     end
