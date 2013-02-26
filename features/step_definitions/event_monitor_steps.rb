8252505bcf3dee32d7707a24ef2b59da32cc654c
James Smith <james@floppy.org.uk>
2013-02-26 11:22:30 +0000

    Rename generic event_details to specific attendee_monitor version

--- features/step_definitions/event_monitor_steps.rb
+++ features/step_definitions/event_monitor_steps.rb
@@ -5,13 +5,13 @@ end
 
 Then /^that event should be queued for attendee checking$/ do
   # Set expectation
-  Resque.should_receive(:enqueue).with(AttendeeMonitor, event_details).once
+  Resque.should_receive(:enqueue).with(AttendeeMonitor, create_attendee_monitor_event_hash).once
   Resque.should_receive(:enqueue).any_number_of_times
 end
 
 Then /^that event should not be queued for attendee checking$/ do
   # Set expectation
-  Resque.should_not_receive(:enqueue).with(AttendeeMonitor, event_details)
+  Resque.should_not_receive(:enqueue).with(AttendeeMonitor, create_attendee_monitor_event_hash)
   Resque.should_receive(:enqueue).any_number_of_times
 end
 
