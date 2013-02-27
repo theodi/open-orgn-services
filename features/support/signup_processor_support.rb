a7ebd7bc64568c2307ccb17ae73dc0da844f72f5
Tom Heath <tom.heath@theodi.org>
2013-02-27 13:58:33 +0000

    massive tidy up of features and reworked signup_processor

--- features/support/signup_processor_support.rb
+++ features/support/signup_processor_support.rb
@@ -1,22 +1,27 @@
 module SignupProcessorSupport
 
   def create_signup_processor_membership_hash
-    member_details =  {
-      :level                 => @level,
-      :organisation_name     => @organisation_name,
-      :contact_name          => @contact_name,
-      :email                 => @invoice_email,
-      :phone                 => @invoice_phone, 
-      :address_line1         => @invoice_address_line1, 
-      :address_line2         => @invoice_address_line2, 
-      :address_city          => @invoice_address_city, 
-      :address_region        => @invoice_address_region, 
-      :address_country       => @invoice_address_country, 
-      :address_postcode      => @invoice_address_postcode, 
-      :tax_number            => @tax_number, 
-      :purchase_order_number => @purchase_order_number,
-      :membership_number     => @membership_number
-    }    
+    # values defined in the step definitions and should be tidied up
+    organization    = {'name' => @company, 'vat_id' => @tax_registration_number}
+    contact_person  = {'name' => @name, 'email' => @email, 'telephone' => @phone}
+    billing         = {
+                        'name' => @name,
+                        'email' => @invoice_email,
+                        'telephone' => @invoice_phone,
+                        'address' => {
+                          'street_address' => @invoice_address_line1,
+                          'address_locality' => @invoice_address_city,
+                          'address_region' => @invoice_address_region,
+                          'address_country' => @invoice_address_country,
+                          'postal_code' => @invoice_address_postcode
+                        }
+                      }
+    purchase        = {
+                        'offer_category' => @membership_level,
+                        'purchase_order_reference' => @purchase_order_reference,
+                        'membership_id' => @membership_id
+                      }
+    [organization, contact_person, billing, purchase]
   end
 
 end
