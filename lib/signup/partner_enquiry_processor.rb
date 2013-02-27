e49a8012401f907af5a756c75e5883603ab91611
James Smith <james@floppy.org.uk>
2013-02-27 08:52:22 +0000

    Add partner enquiry processor stub
    No tests at the moment, some will be added in member-directory, and some here later.

---
+++ lib/signup/partner_enquiry_processor.rb
@@ -0,0 +1,23 @@
+class PartnerEnquiryProcessor
+  @queue = :signup
+  
+  # Public: Process new enquiries
+  #
+  # enquiry_details - a hash containing details of the enquiry.
+  #                   'name'         => the name of the enquirer
+  #                   'affiliation'  => the person's organisation
+  #                   'job_title'    => role
+  #                   'email'        => email address
+  #                   'telephone'    => phone number
+  #                   'note'         => free text, 'what would you like to talk about'
+  #
+  # Examples
+  #
+  #   SignupProcessor.perform({:name => 'Bob Fish', :affiliation => 'New Company Inc.', ...})
+  #   # => nil
+  #
+  # Returns nil. Queues CRM task creation jobs.
+  def self.perform(enquiry_details)
+  end
+  
+end
