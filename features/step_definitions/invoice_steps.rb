f1661df92d0210dca4be82c7bd2f1764123cdb7c
James Smith <james@floppy.org.uk>
2013-03-01 16:31:09 +0000

    Update invoicer steps to not assume knowledge of events

--- features/step_definitions/invoice_steps.rb
+++ features/step_definitions/invoice_steps.rb
@@ -2,6 +2,18 @@ Given /^my order number is (#{INTEGER})$/ do |order_number|
   @order_number = order_number
 end
 
+Given /^the invoice is due on (#{DATE})$/ do |date|
+@invoice_due_date = date
+end
+
+Given /^the invoice amount is (.*?)$/ do |base_price|
+  @base_price = base_price
+end
+
+Given /^the invoice description is "(.*?)"$/ do |invoice_description|
+  @invoice_description = invoice_description
+end
+
 # loads of duplication here
 # deliberately left in to prompt us to converge on phrasing style
 
