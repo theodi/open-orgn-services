2939f198f092b2b0071529629bf4bdad76b8fc8e
James Smith <james@floppy.org.uk>
2013-03-06 15:32:27 +0000

    Requeue signups that haven't been synced yet

--- features/step_definitions/signup_to_capsule_steps.rb
+++ features/step_definitions/signup_to_capsule_steps.rb
@@ -1,5 +1,13 @@
 Then /^my signup should be requeued for later processing once the contact has synced from Xero$/ do
-  pending # express the regexp above with the code you wish you had
+  membership  = {
+    'product_name' => @product_name,
+    'number'       => @membership_number,
+    'join_date'    => @join_date
+  }
+  organization = {
+    'name' => @company
+  }
+  Resque.should_receive(:enqueue_in).with(1.hour, SendSignupToCapsule, organization, membership).once
 end
 
 When /^I sign up via the website$/ do
