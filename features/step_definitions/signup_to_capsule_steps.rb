4a68f46b486db06b8a7d24b99e3d8e9a7ebbc0d0
James Smith <james@floppy.org.uk>
2013-03-06 15:17:27 +0000

    Add pending steps for new features

--- features/step_definitions/signup_to_capsule_steps.rb
+++ features/step_definitions/signup_to_capsule_steps.rb
@@ -1,3 +1,11 @@
+Then /^my signup should be requeued for later processing once the contact has synced from Xero$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I sign up via the website$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
 Then /^I should be added to the capsulecrm queue$/ do
   
 end
---
+++ features/step_definitions/website_signup_steps.rb
@@ -0,0 +1,11 @@
+Given /^I signed up for membership at the level called "(.*?)"$/ do |level|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I signed up on (#{DATE})$/ do |date|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^my membership number is "(.*?)"$/ do |membership_number|
+  pending # express the regexp above with the code you wish you had
+end
