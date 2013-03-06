2b733898d41728f0ccbd5f0435807f1fc2707851
James Smith <james@floppy.org.uk>
2013-03-06 17:09:56 +0000

    Create opportunity for signup
    Untested, I'm on a train.

--- lib/signup/send_signup_to_capsule.rb
+++ lib/signup/send_signup_to_capsule.rb
@@ -18,6 +18,22 @@ class SendSignupToCapsule
     organisation = CapsuleCRM::Organisation.find_all(:q => organization['name']).first
     if organisation.nil?
       Resque.enqueue_in(1.hour, SendSignupToCapsule, organization, membership)
+    else
+      # Create opportunity against organisation
+      opportunity = CapsuleCRM::Opportunity.new(
+        :party_id => organisation.id, 
+        :name => "Membership at #{membership['product_name']} level",
+        :currency => 'GBP',
+        :description => "Membership #: #{membership['number']}",
+        # :value => value_for_product_name(membership['product_name']),
+        # :duration => 12,
+        # :duration_basis => 'MONTH',
+        :milestone => 'Won',
+        :probability => 100,
+        :expected_close_date => Date.parse(membership['join_date']),
+        :owner => ENV['CAPSULECRM_DEFAULT_OWNER'],
+      )
+      opportunity.save
     end
   end
   
