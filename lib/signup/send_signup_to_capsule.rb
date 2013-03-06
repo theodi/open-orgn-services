6dd75076c7d5ca2b638e11a60666dcfe0f2a86d4
James Smith <james@floppy.org.uk>
2013-03-06 15:34:50 +0000

    Add tomdoc

--- lib/signup/send_signup_to_capsule.rb
+++ lib/signup/send_signup_to_capsule.rb
@@ -5,7 +5,14 @@ class SendSignupToCapsule
   
   # Public: Store details of self-signups in CapsuleCRM
   #
-  #
+  # membership   - a hash containing details of the new membership
+  #              'product_name' => the membership level
+  #              'number'       => the newly-generated membership number
+  #              'join_date'    => the date of signup
+  # 
+  # organization - a hash containing details of the organization
+  #              'name' => the org name in Xero - should be the same as that in capsule
+  # 
   # Returns nil.
   def self.perform(organization, membership)
     organisation = CapsuleCRM::Organisation.find_all(:q => organization['name']).first
