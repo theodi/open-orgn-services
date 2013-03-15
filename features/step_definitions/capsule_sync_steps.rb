82d266e4f1a5833c74ef723212f74349baa18807
James Smith <james@floppy.org.uk>
2013-03-15 16:39:00 +0000

    Add sync task to read from capsule organization and call back to observers

--- features/step_definitions/capsule_sync_steps.rb
+++ features/step_definitions/capsule_sync_steps.rb
@@ -1,3 +1,19 @@
+Given /^that data tag has the following fields:$/ do |table|
+  table.hashes.each do |row|
+    row.each_pair do |key, value|      
+      instance_variable_set("@organization_#{key.downcase}", value)
+      field = CapsuleCRM::CustomField.new(
+        @organisation,
+        :tag => @tag.name,
+        :label => key,
+        :text => value,
+        :boolean => (value == "true")
+      )
+      field.save
+    end
+  end
+end
+
 Then /^that organisation should be queued for sync$/ do
   Resque.should_receive(:enqueue).with(SyncCapsuleData, @organisation.id)
 end
@@ -8,4 +24,32 @@ end
 
 When /^the capsule monitor runs$/ do
   CapsuleSyncMonitor.perform
+end
+
+class MyObserverClass
+  def self.register
+    SyncCapsuleData.add_observer(self)
+  end
+  def self.update(data)
+  end
+end
+
+Given /^an observer object has been registered$/ do
+  MyObserverClass.register
+end
+
+When /^the capsule sync job for that organisation runs$/ do
+  SyncCapsuleData.perform(@organisation.id)
+end
+
+Then /^the observer should be notified with the organisation's information$/ do
+  data = {
+    'active'      => @organization_active,
+    'email'       => @organization_email,
+    'name'        => @organisation.name,
+    'description' => @organization_description,
+    'url'         => @organization_homepage,
+    'product_name'=> @organization_level,
+  }
+  MyObserverClass.should_receive(:update).with(data)
 end
