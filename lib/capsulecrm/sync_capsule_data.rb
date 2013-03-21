8b922bad880c3b7ee05256461ec6e56b1e2e95c9
James Smith <james@floppy.org.uk>
2013-03-21 10:46:33 +0000

    separate hashes sent to observer

--- lib/capsulecrm/sync_capsule_data.rb
+++ lib/capsulecrm/sync_capsule_data.rb
@@ -11,28 +11,26 @@ class SyncCapsuleData
     # Get the organization from capsule
     org = CapsuleCRM::Organisation.find(organization_id)
     if org
-      data = {
-        'membership' => {
-          'email'         => field(org, "Membership", "Email").try(:text),
-          'product_name'  => field(org, "Membership", "Level").try(:text),
-          'id'            => field(org, "Membership", "ID").try(:text),
-        }.compact,
-        'directory_entry' => {
-          'active'        => field(org, "DirectoryEntry", "Active").try(:boolean).try(:to_s),
-          'name'          => org.name,
-          'description'   => field(org, "DirectoryEntry", "Description").try(:text),
-          'url'           => field(org, "DirectoryEntry", "Homepage").try(:text),
-          'contact'       => field(org, "DirectoryEntry", "Contact").try(:text),
-          'phone'         => field(org, "DirectoryEntry", "Phone").try(:text),
-          'email'         => field(org, "DirectoryEntry", "Email").try(:text),
-          'twitter'       => field(org, "DirectoryEntry", "Twitter").try(:text),
-          'linkedin'      => field(org, "DirectoryEntry", "Linkedin").try(:text),
-          'facebook'      => field(org, "DirectoryEntry", "Facebook").try(:text),          
-        }.compact,
-      }
+      membership = {
+        'email'         => field(org, "Membership", "Email").try(:text),
+        'product_name'  => field(org, "Membership", "Level").try(:text),
+        'id'            => field(org, "Membership", "ID").try(:text),
+      }.compact
+      directory_entry = {
+        'active'        => field(org, "DirectoryEntry", "Active").try(:boolean).try(:to_s),
+        'name'          => org.name,
+        'description'   => field(org, "DirectoryEntry", "Description").try(:text),
+        'url'           => field(org, "DirectoryEntry", "Homepage").try(:text),
+        'contact'       => field(org, "DirectoryEntry", "Contact").try(:text),
+        'phone'         => field(org, "DirectoryEntry", "Phone").try(:text),
+        'email'         => field(org, "DirectoryEntry", "Email").try(:text),
+        'twitter'       => field(org, "DirectoryEntry", "Twitter").try(:text),
+        'linkedin'      => field(org, "DirectoryEntry", "Linkedin").try(:text),
+        'facebook'      => field(org, "DirectoryEntry", "Facebook").try(:text),          
+      }.compact
       # Notify the observers
       changed
-      notify_observers(data)
+      notify_observers(membership, directory_entry)
     end
   end
   
