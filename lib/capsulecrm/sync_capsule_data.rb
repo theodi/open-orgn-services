d62c774c83191b53b9de3b32a9b4cca6e89ffe42
James Smith <james@floppy.org.uk>
2013-03-21 06:58:57 +0000

    sync directory entry data back from capsulecrm

--- lib/capsulecrm/sync_capsule_data.rb
+++ lib/capsulecrm/sync_capsule_data.rb
@@ -12,14 +12,24 @@ class SyncCapsuleData
     org = CapsuleCRM::Organisation.find(organization_id)
     if org
       data = {
-        'product_name'  => field(org, "Membership", "Level").try(:text),
-        'email'         => field(org, "Membership", "Email").try(:text),
-        'membership_id' => field(org, "Membership", "ID").try(:text),
-        'name'          => org.name,
-        'active'        => field(org, "DirectoryEntry", "Active").try(:boolean).try(:to_s),
-        'description'   => field(org, "DirectoryEntry", "Description").try(:text),
-        'url'           => field(org, "DirectoryEntry", "Homepage").try(:text),
-      }.compact
+        'membership' => {
+          'email'         => field(org, "Membership", "Email").try(:text),
+          'product_name'  => field(org, "Membership", "Level").try(:text),
+          'membership_id' => field(org, "Membership", "ID").try(:text),
+        }.compact,
+        'directory_entry' => {
+          'active'        => field(org, "DirectoryEntry", "Active").try(:boolean).try(:to_s),
+          'name'          => org.name,
+          'description'   => field(org, "DirectoryEntry", "Description").try(:text),
+          'url'           => field(org, "DirectoryEntry", "Homepage").try(:text),
+          'contact'       => field(org, "DirectoryEntry", "Contact").try(:text),
+          'phone'         => field(org, "DirectoryEntry", "Phone").try(:text),
+          'email'         => field(org, "DirectoryEntry", "Email").try(:text),
+          'twitter'       => field(org, "DirectoryEntry", "Twitter").try(:text),
+          'linkedin'      => field(org, "DirectoryEntry", "Linkedin").try(:text),
+          'facebook'      => field(org, "DirectoryEntry", "Facebook").try(:text),          
+        }.compact,
+      }
       # Notify the observers
       changed
       notify_observers(data)
