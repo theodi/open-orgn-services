2f28e60a13f5fb3b1f45cb059af19a39fa3ccbd4
James Smith <james@floppy.org.uk>
2013-07-18 11:23:52 +0100

    Absorb github errors in all github monitor jobs

--- lib/github/watchers_forks_monitor.rb
+++ lib/github/watchers_forks_monitor.rb
@@ -15,6 +15,12 @@ class Github::WatchersForksMonitor
     # Push into leftronic
     Resque.enqueue LeftronicPublisher, :number, ENV['LEFTRONIC_GITHUB_WATCHERS'], watchers
     Resque.enqueue LeftronicPublisher, :number, ENV['LEFTRONIC_GITHUB_FORKS'], forks
+  rescue Faraday::Error::TimeoutError, Faraday::Error::ConnectionFailed
+    # We sometimes get oauth timeouts on these requests. Let's just absorb them quietly and wait for the next time round.
+    nil
+  rescue Github::Error::InternalServerError, Github::Error::ServiceError
+    # Sometimes Github gives us server errors too. Let's ignore those as well.
+    nil
   end
   
 end
