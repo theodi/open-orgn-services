7bf78babd6458376ae892135473545b465ad1fdc
James Smith <james@floppy.org.uk>
2013-12-11 12:53:28 +0000

    change env var for metrics API base URL

--- features/step_definitions/metrics_steps.rb
+++ features/step_definitions/metrics_steps.rb
@@ -38,7 +38,7 @@ Then(/^the following data should be stored in the "(.*?)" metric$/) do |metric, 
   json = "{\"name\":\"#{metric}\",\"time\":\"#{DateTime.now.xmlschema}\",\"value\":#{string.gsub(/\s+/, "")}}"
   auth = {:username => ENV['METRICS_API_USERNAME'], :password => ENV['METRICS_API_PASSWORD']}
   HTTParty.should_receive(:post).
-      with("#{ENV['METRICS_BASE_URL']}metrics/#{metric}", :body => json, :headers => { 'Content-Type' => 'application/json' },
+      with("#{ENV['METRICS_API_BASE_URL']}metrics/#{metric}", :body => json, :headers => { 'Content-Type' => 'application/json' },
           :basic_auth => auth).
       once
 end
