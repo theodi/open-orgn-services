1ed93df89dcb33dcc9b85f051593daa00c47fc7a
James Smith <james@floppy.org.uk>
2013-12-16 07:48:29 +0000

    Fix JSON format tests

--- features/step_definitions/metrics_steps.rb
+++ features/step_definitions/metrics_steps.rb
@@ -35,7 +35,7 @@ Then /^html containing '(.*?)' should be stored in the (.*?) stat$/ do |content,
 end
 
 Then(/^the following data should be stored in the "(.*?)" metric$/) do |metric, value|
-  json = "{\"name\":\"#{metric}\",\"time\":\"#{DateTime.now.xmlschema}\",\"value\":#{value.is_a?(String) ? value.gsub(/\s+/, "") : value}}"
+  json = JSON.parse("{\"name\":\"#{metric}\",\"time\":\"#{DateTime.now.xmlschema}\",\"value\":#{value}}").to_json
   auth = {:username => ENV['METRICS_API_USERNAME'], :password => ENV['METRICS_API_PASSWORD']}
   HTTParty.should_receive(:post).
       with("#{ENV['METRICS_API_BASE_URL']}metrics/#{metric}", :body => json, :headers => { 'Content-Type' => 'application/json' },
