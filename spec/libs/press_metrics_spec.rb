05b71c3b7bc9307fac34bcaf040105f2689b7c7a
James Smith <james@floppy.org.uk>
2014-04-08 14:16:48 +0100

    rewrite press metrics spec

--- spec/libs/press_metrics_spec.rb
+++ spec/libs/press_metrics_spec.rb
@@ -7,21 +7,16 @@ describe PressMetrics do
   end
 
   it "should store right values in metrics API" do
-    WebMock.allow_net_connect!
-    time = DateTime.now
-    {
-      "current-year-press-sentiment"              => '{"positive":412,"neutral":15,"balanced":14,"negative":1}',
-      "current-year-press-spokespeople"           => '{"Tim Berners-Lee":18,"Nigel Shadbolt":16,"Gavin Starks":15,"Jeni Tennison":11,"Kathryn Corrick":6,"Emma Thwaites":4,"Ulrich Atz":3,"Richard Stirling":3,"Waldo Jaquith":3}',
-      "current-year-press-sector-spread"          => '{"Technology":332,"Government":328,"Corporate":136,"Cultural":53,"Financial":14,"Retail":5}',
-      "current-year-press-geographical-spread"    => '{"UK":522,"Unspecified":88,"USA":53,"Canada":31,"Germany":9,"Italy":9,"Australia":6,"Singapore":4,"Belgium":3,"India":3,"France":2,"Africa":2,"Denmark":2,"Europe":1,"Brazil":1,"Austria":1,"Columbia":1,"Taiwan":1,"Nigeria":1,"Malaysia":1,"Malta":1}',
-      "current-year-press-totals"                 => '{"ODI":{"volume":440,"value":1495674,"reach":57628683},"OpenData":{"volume":72,"value":417652,"reach":23181210}}',
-    }.each_pair do |metric, value|
-      metrics_api_should_receive metric, time, value
-    end
-
+    # Which methods are called?
+    PressMetrics.should_receive(:sentiment).once
+    PressMetrics.should_receive(:spokespeople).once
+    PressMetrics.should_receive(:sector_spread).once
+    PressMetrics.should_receive(:geographical_spread).once
+    PressMetrics.should_receive(:totals).once
+    # How many metrics are stored?
+    PressMetrics.should_receive(:store_metric).exactly(5).times
+    # Do it
     PressMetrics.perform
-    Timecop.return
-    WebMock.disable_net_connect!
   end
 
   it "should show the correct sentiment values", :vcr do
