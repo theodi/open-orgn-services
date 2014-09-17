163c5976c451989e23a6a3fa30f18bc441754567
James Smith <james@floppy.org.uk>
2014-09-17 15:00:09 +0100

    helpful comment

--- lib/capsulecrm/capsule_sync_monitor.rb
+++ lib/capsulecrm/capsule_sync_monitor.rb
@@ -8,6 +8,7 @@ class CapsuleSyncMonitor
     orgs = CapsuleCRM::Organisation.find_all(:tag => "Membership")
     orgs.each do |org|
       # Check if data tags have been updated
+      # Updated at time should be more recent than 2 hours ago
       if org.updated_at > 2.hours.ago
         Resque.enqueue(SyncCapsuleData, org.id)
       end
