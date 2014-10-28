2b6afc823729d9eae907c934f799ee07c48780ca
Adam Sven Johnson <adam@pkqk.net>
2014-10-28 15:26:16 +0000

    Read stats from datasets published certificate count

--- spec/libs/certificate_count_spec.rb
+++ spec/libs/certificate_count_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe CertificateCount do
   it "should show the correct number of published Open Data Certificates", :vcr do
-    CertificateCount.odcs.should == 637
+    CertificateCount.odcs.should == 2618
   end
 
   it "should store right values in metrics API", :vcr do
     Timecop.freeze
     time = DateTime.now
-    metrics_api_should_receive("certificated-datasets", time, 637)
+    metrics_api_should_receive("certificated-datasets", time, 2618)
 
     CertificateCount.perform
     Timecop.return
