5fe99f53f11dbf4ffa4a66064de5acc5c754a6c7
James Smith <james@floppy.org.uk>
2014-11-11 10:56:14 +0000

    Remove dashboard time job

--- lib/leftronic/dashboard_time.rb
+++
@@ -1,9 +0,0 @@
-class DashboardTime
-  
-  @queue = :metrics
-  
-  def self.perform
-    Resque.enqueue LeftronicPublisher, :html, ENV['LEFTRONIC_JENKINS_TIME'], "<div>#{DateTime.now.to_s}</div>"
-  end
-
-end
