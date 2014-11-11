ac1078f2d7aab35feb05b271c27c085ca0f1e72d
James Smith <james@floppy.org.uk>
2014-11-11 17:37:57 +0000

    Remove hubot PR posting
    Never worked properly and there are github hooks these days

--- lib/github/pull_request_monitor.rb
+++ lib/github/pull_request_monitor.rb
@@ -13,18 +13,6 @@ class Github::PullRequestMonitor
     pull_requests = 0
     github.repos.list(user: ENV['GITHUB_ORGANISATION']) do |repo|
       open_pulls = github.pulls.list(ENV['GITHUB_ORGANISATION'], repo.name)
-      last_updated = DateTime.parse(Resque.redis.get("#{repo.name}")) rescue nil
-      open_pulls.each do |pull|
-        if last_updated.nil? || last_updated <= DateTime.parse(pull['created_at'])
-          HTTParty.post("#{ENV['HUBOT_URL']}/pull-requests", :body => {
-              :url => pull['html_url'],
-              :repo => repo.name,
-              :title => pull['title']         
-            }.to_json,
-            :headers => { 'Content-Type' => 'application/json' } )
-            Resque.redis.set("#{repo.name}", DateTime.now)
-        end 
-      end
       open_pull_requests += open_pulls.count
       pull_requests += open_pulls.count
       closed_pulls = github.pulls.list(ENV['GITHUB_ORGANISATION'], repo.name, state: 'closed')
