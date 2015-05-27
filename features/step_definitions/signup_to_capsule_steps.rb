1a10b386c747c7e2f0bc5e0544ca367ea157bd83
pezholio <pezholio@gmail.com>
2015-05-27 14:22:30 +0100

    Remove steps we don't need

--- features/step_definitions/signup_to_capsule_steps.rb
+++ features/step_definitions/signup_to_capsule_steps.rb
@@ -1,21 +1,3 @@
-Then /^my signup should be requeued for later processing once the contact has synced from Xero$/ do
-  organization = {
-    'name' => @company,
-    'company_number' => @company_number,
-    'email' => @email || @invoice_email
-  }
-  membership  = {
-    'product_name'    => @membership_level,
-    'supporter_level' => "Supporter",
-    'id'              => @membership_id.to_s,
-    'join_date'       => Date.today.to_s,
-    'contact_email'   => @email,
-    'size'            => @size,
-    'sector'          => @sector
-  }
-  Resque.should_receive(:enqueue_in).with(1.hour, SendSignupToCapsule, organization, membership).once
-end
-
 When /^I sign up via the website$/ do
   organization = {
     'name' => @company || @contact_name,
@@ -32,24 +14,6 @@ When /^I sign up via the website$/ do
     'sector'          => @sector
   }.compact
   SendSignupToCapsule.perform(organization, membership)
-end
-
-Then /^I should be added to the capsulecrm queue$/ do
-  organization = {
-    'name' => @company,
-    'company_number' => @company_number,
-    'email' => @invoice_email
-  }.compact
-  membership  = {
-    'product_name'     => @membership_level,
-    'supporter_level'  => @supporter_level,
-    'id'               => @membership_id.to_s,
-    'join_date'        => Date.today.to_s,
-    'contact_email'    => @email,
-    'size'             => @size,
-    'sector'           => @sector
-  }.compact
-  Resque.should_receive(:enqueue).with(SendSignupToCapsule, organization, membership).once
 end
 
 Then(/^my organisation contact details should exist in CapsuleCRM$/) do
