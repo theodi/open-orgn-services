eb2cf84795ee340379adadc42d91e2bde5f9a879
Jordan Elver <jordan.elver@gmail.com>
2015-08-14 13:54:48 +0100

    Start adding SignupProcessor tests
    Starting with `#description` as it's easiest! :P

---
+++ spec/signup_processor_spec.rb
@@ -0,0 +1,55 @@
+require "active_support/inflector"
+
+require_relative "../lib/signup/signup_processor"
+
+describe SignupProcessor do
+  subject do
+    SignupProcessor.new(organization, contact_person, billing, purchase)
+  end
+
+  let(:organization) {}
+
+  let(:contact_person) {}
+
+  let(:billing) {}
+
+  let(:purchase) {}
+
+  describe "#description" do
+    let(:membership_id) { "12345" }
+    let(:description) { "Individual supporter" }
+    let(:type) { "individual" }
+    let(:frequency) { "annual" }
+
+    context "payment method is credit card" do
+      let(:method) { "credit_card" }
+
+      it "returns the correct description" do
+        expect(subject.description(membership_id, description, type, method, frequency)).to eq(
+          "ODI Individual supporter (12345) [Individual] (annual card payment)"
+        )
+      end
+    end
+
+    context "payment method is direct debit" do
+      let(:method) { "direct_debit" }
+
+      it "returns the correct description" do
+        expect(subject.description(membership_id, description, type, method, frequency)).to eq(
+          "ODI Individual supporter (12345) [Individual] (annual dd payment)"
+        )
+      end
+    end
+
+    context "payment method is something other than credit card or direct debit" do
+      let(:method) { "invoice" }
+
+      it "returns the correct description" do
+        expect(subject.description(membership_id, description, type, method, frequency)).to eq(
+          "ODI Individual supporter (12345) [Individual] (annual invoice payment)"
+        )
+      end
+    end
+  end
+end
+
