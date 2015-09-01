36e064c0616691b9d02c5fdc480566361d691920
Jordan Elver <jordan.elver@gmail.com>
2015-09-01 12:40:20 +0100

    Add active flag to the data sent to Capsule

--- lib/capsulecrm/directory_entry_processor.rb
+++ lib/capsulecrm/directory_entry_processor.rb
@@ -11,6 +11,7 @@ class SendDirectoryEntryToCapsule
   #                   'name'         => the organisation name
   #
   # directory_entry   - a hash containing details of the directory entry
+  #                   'active'       => whether the organization should be active or not
   #                   'description'  => the description that will appear against the
   #                                     organisation in the business directory
   #                   'homepage'     => the website address of the business
@@ -40,6 +41,7 @@ class SendDirectoryEntryToCapsule
       description = directory_entry['description'].scan(/.{1,250}/)
       # Store
       fields = {
+        'Active'        => directory_entry['active'],
         'Description'   => description[0],
         'Description-2' => description[1],
         'Description-3' => description[2],
