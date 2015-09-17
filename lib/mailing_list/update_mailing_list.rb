01b358c8b6dc629aa5d8ef18f04eb85d673d9f7c
Jordan Elver <jordan.elver@gmail.com>
2015-09-17 14:12:44 +0100

    Add job to actually update each member
    These are queued from the `SyncMailingList` job

---
+++ lib/mailing_list/update_mailing_list.rb
@@ -0,0 +1,44 @@
+require "active_support/core_ext/string/inflections"
+
+class UpdateMailingList
+  @queue = :mailing_list
+
+  def self.perform(id, type)
+    new(id, type).perform
+  end
+
+  attr_writer :mailing_list, :crm
+  attr_reader :id
+
+  def initialize(id, type)
+    @id   = id
+    @type = type
+  end
+
+  def perform
+    if member.newsletter?
+      mailing_list.subscribe(member.email)
+    else
+      mailing_list.unsubscribe(member.email)
+    end
+  end
+
+  private
+
+  def member
+    @member ||= crm.find(type, id)
+  end
+
+  def type
+    @type.to_sym
+  end
+
+  def crm
+    @crm ||= CRM
+  end
+
+  def mailing_list
+    @mailing_list ||= MailingList
+  end
+end
+
---
+++ spec/update_mailing_list_spec.rb
@@ -0,0 +1,47 @@
+require_relative "../lib/mailing_list/update_mailing_list"
+
+describe UpdateMailingList do
+
+  subject do
+    UpdateMailingList.new(id, type)
+  end
+
+  let(:id) { 123 }
+  let(:type) { "Person" }
+  let(:mailing_list) { double("Mailing list") }
+  let(:member) { double("Member", email: "test@example.com") }
+  let(:crm) { double("CRM") }
+
+  before do
+    subject.mailing_list = mailing_list
+    subject.crm = crm
+  end
+
+  describe "#perform" do
+
+    before do
+      allow(crm).to receive(:find).and_return(member)
+    end
+
+    context "member doesn't want to receive the newsletter" do
+      it "unsubscribes the user from the list" do
+        allow(member).to receive(:newsletter?).and_return(true)
+
+        expect(mailing_list).to receive(:subscribe).with("test@example.com")
+
+        subject.perform
+      end
+    end
+
+    context "member does want to receive the newsletter" do
+      it "subscribes the user to the list" do
+        allow(member).to receive(:newsletter?).and_return(false)
+
+        expect(mailing_list).to receive(:unsubscribe).with("test@example.com")
+
+        subject.perform
+      end
+    end
+  end
+end
+
