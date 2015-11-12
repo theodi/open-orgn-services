6f46cb5397a211b12705ea0236df22f7119998be
Jordan Elver <jordan.elver@gmail.com>
2015-11-12 12:28:06 +0000

    Extract row into separate object

--- lib/chargify/booking_value_report.rb
+++ lib/chargify/booking_value_report.rb
@@ -16,18 +16,56 @@ module Reports
     def data
       table = []
       table << headers
+
       @products.values.each do |product|
-        count = @transactions.count { |txn| txn.type == 'Payment' && txn.product_id == product.id }
-        amount = if product.handle =~ /monthly$/
-          product.price_in_cents*12/100
-        else
-          product.price_in_cents/100
-        end
-        net = amount*count
-        total, tax = 1.2*net, 0.2*net
-        table << [product.handle, count.to_s, amount.to_i.to_s, net.to_i.to_s, tax.to_i.to_s, total.to_i.to_s]
+        table << Row.new(@transactions, product).row
       end
+
       table
+    end
+
+    class Row
+      attr_reader :transactions, :product
+
+      def initialize(transactions, product)
+        @transactions = transactions
+        @product = product
+      end
+
+      def count
+        transactions.count { |txn| txn.type == 'Payment' && txn.product_id == product.id }
+      end
+
+      def amount
+        if product.handle =~ /monthly$/
+          product.price_in_cents * 12 / 100
+        else
+          product.price_in_cents / 100
+        end
+      end
+
+      def net
+        amount * count
+      end
+
+      def total
+        1.2 * net
+      end
+
+      def tax
+        0.2 * net
+      end
+
+      def row
+        [
+          product.handle,
+          count.to_s,
+          amount.to_i.to_s,
+          net.to_i.to_s,
+          tax.to_i.to_s,
+          total.to_i.to_s
+        ]
+      end
     end
   end
 end
